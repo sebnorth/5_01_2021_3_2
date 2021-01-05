99108ad94d176f9e43cdb554e648a65a61c74d2f
sebnorth <sebnorth@gmail.com>
2021-01-05 09:08:39 +0100

    dopisałem funkcję rekurencyjną f

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie2
+{
+    class Program
+    {
+
+        static int f(int n) {
+
+            if (n < 1) return n;
+            else if (n == 1) return 1;
+            else return 2 * f(n - 1) + 1;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(f(3)); // 7
+            Console.WriteLine(f(f(3))); // 127
+
+            Console.ReadKey();
+
+        }
+    }
+}
